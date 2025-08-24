4d92657a8ec853f5c0f63432a285dd39e99199e6
Artis Kaposts <kaposts.artis1@gmail.com>
2025-08-24 20:38:15 +0300

    Implemented bullets

---
+++ scripts/bullets/bullet.gd
@@ -0,0 +1,52 @@
+extends Area2D
+class_name Bullet
+
+@export var data: BulletData
+@onready var sprite: Sprite2D = $Sprite
+
+var direction: Vector2 = Vector2.RIGHT
+var time_alive: float = 0.0
+var remaining_pierce: int = 0
+
+func _ready() -> void:
+	remaining_pierce = data.pierce_count
+	
+	if data.texture:
+		sprite.texture = data.texture
+		scale = Vector2.ONE * data.scale
+
+	if data.trail_vfx:
+		var trail: Node = data.trail_vfx.instantiate()
+		add_child(trail)
+
+func _physics_process(delta: float) -> void:
+	position += direction * data.speed * delta
+	time_alive += delta
+	
+	if time_alive >= data.lifetime:
+		queue_free()
+
+func _on_body_entered(body: Node) -> void:
+	if "apply_damage" not in body: 
+		return
+	else: 
+		body.apply_damage(data.damage, data.knockback, global_position, direction)
+	
+	if data.hit_vfx:
+		var hit_vfx: GPUParticles2D = data.hit_vfx.instantiate()
+		hit_vfx.global_position = global_position
+		hit_vfx.emitting = true
+		hit_vfx.connect("finished", Callable(hit_vfx, "queue_free"))
+		get_parent().add_child(hit_vfx)
+
+	if data.hit_sfx:
+		var audio: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+		audio.stream = data.hit_sfx
+		audio.global_position = global_position
+		get_parent().add_child(audio)
+		audio.play()
+
+	if remaining_pierce > 0:
+		remaining_pierce -= 1
+	else:
+		queue_free()
--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 class_name PlayerController
 
+#region movement_variables
 @export var speed: float = 220.0
 @export var accel: float = 1500.0
 @export var friction: float = 2000.0
+#endregion
+
+#region shooting_variables
+@export var bullet_scene: PackedScene = preload("res://scenes/bullets/bullet.tscn")
+@export var bullet_data: BulletData = preload("res://resources/bullets/first_bullet.tres")
+@onready var cooldown_timer: Timer = $Timer
+@export var bullets_per_shot: int = 1
+const spread_angle_multiplier: int = 10
+#endregion
 
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	handle_movement(delta)
+	
+	handle_shooting()
 
 func handle_movement(delta: float) -> void:
 	var input_dir: Vector2 = Vector2(
@@ -27,3 +39,37 @@ func handle_movement(delta: float) -> void:
 			velocity = Vector2.ZERO
 
 	move_and_slide()
+
+func handle_shooting() -> void:
+	if cooldown_timer.is_stopped():
+		shoot(get_global_mouse_position())
+		cooldown_timer.start()
+
+func shoot(to_world: Vector2) -> void:
+	if bullets_per_shot <= 0:
+		return
+	
+	## 1 bullet = no angle
+	## 3 bullet = 20 degree angle
+	## 5 bullet = 40 degree angle
+	var spread_angle: float = spread_angle_multiplier * (bullets_per_shot - 1)
+	
+	var base_dir: Vector2 = (to_world - global_position).normalized()
+	var base_angle: float = base_dir.angle()  # in radians
+	
+	# calculate angle between bullets
+	var step_angle: float = 0.0
+	if bullets_per_shot > 1:
+		step_angle = deg_to_rad(spread_angle) / (bullets_per_shot - 1)
+	
+	var start_angle: float = base_angle - deg_to_rad(spread_angle) / 2
+	
+	for i: int in range(bullets_per_shot):
+		var bullet: Area2D = bullet_scene.instantiate()
+		bullet.data = bullet_data
+		bullet.global_position = global_position
+
+		var angle: float = start_angle + step_angle * i
+		bullet.direction = Vector2(cos(angle), sin(angle))
+
+		get_tree().current_scene.add_child(bullet)
