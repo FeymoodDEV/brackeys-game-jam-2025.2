32b9e462eac44acfb44dab516e787aadcb843928
Artis Kaposts <kaposts.artis1@gmail.com>
2025-08-26 01:54:55 +0300

    Added resource script for blocks

--- scripts/block/block.gd
+++ scripts/block/block.gd
@@ -1,5 +1,23 @@
 extends Node
 class_name Block
 
+@export var data: BlockData
+
+var health: int
+var isDestructable: bool = true
+@onready var sprite: Sprite2D = $Sprite
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	health = data.health
+	isDestructable = data.isDestructable
+	sprite.texture = data.texture
+
 func apply_damage(damage: int, knockback: float, global_position: Vector2, direction: Vector2):
-	print('Block was hit')
+	anim.play("hit")
+	
+	if !isDestructable: return
+	
+	health -= 1
+	if health <= 0:
+		queue_free()
