f675603e1a87374d92e5d28f7a3c6f26c60acbeb
Nathan Estrada <naestrada19@gmail.com>
2025-08-27 17:17:41 -0700

    Working on scene-loader

---
+++ scripts/managers/bullet_manager.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var player_node = $Player;
+
+func _ready():
+	deferred_ready.call_deferred();
+	
+func deferred_ready():
+	player_node.reparent(get_parent())
+	MainBus.player_spawned.emit(player_node.get_path())
--- src/main_loader.gd
+++ src/main_loader.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var packed_bullet_manager: PackedScene;
 @export var packed_menu_scene: PackedScene;
 @export var packed_ui_scene: PackedScene;
+@export var packed_player_scene: PackedScene;
 
 @export_category("Debug Variables")
 @export var DebugMode: bool;
@@ -12,6 +13,7 @@ extends Node2D
 var bullet_manager: Node2D #TODO Create class type for this
 var menu_scene: Node2D;
 var testing_scene_node: Node2D;
+var player_node: Node2D;
 
 func _init():
 	pass
@@ -24,14 +26,25 @@ func _ready():
 	
 	if packed_menu_scene:
 		menu_scene = packed_menu_scene.instantiate();
+		
+	if packed_player_scene:
+		player_node = packed_player_scene.instantiate();
 	
-	pass
 	
 	if is_instance_valid(bullet_manager):
 		add_child(bullet_manager);
+	else:
+		push_warning("BulletManager node invalid")
 		
 	if is_instance_valid(menu_scene):
 		add_child(menu_scene);
+	else:
+		push_warning("MenuScene node invalid!")
+		
+	if is_instance_valid(player_node):
+		add_child(player_node);
+	else:
+		push_warning("PlayerNode invalid")
 	
 	if DebugMode:
 		add_child(testing_scene_node);
