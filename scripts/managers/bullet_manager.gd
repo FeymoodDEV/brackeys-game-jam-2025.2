e66ca1935b6df376cd9171bb9e4f1ab98b97c7c1
Nathan Estrada <naestrada19@gmail.com>
2025-08-27 20:48:49 -0700

    More direct tweaks to the addon and core systems

--- scripts/managers/bullet_manager.gd
+++ scripts/managers/bullet_manager.gd
@@ -6,4 +6,6 @@ func _ready():
 	deferred_ready.call_deferred();
 	
 func deferred_ready():
+	Spawning.create_pool("PBullet", "0", 200, true);
+	Spawning.create_pool("EBullet", "1", 200, true);
 	player_node.reparent(get_parent())
