f37bded62114afda5afb1760fcb7070bf6ed27b3
Feymood <feymood@pm.me>
2025-08-29 15:48:14 +0200

    Add shot speck VFX and death VFX

--- addons/BulletUpHell/customFunctions.gd
+++ addons/BulletUpHell/customFunctions.gd
@@ -8,7 +8,8 @@ class_name customFunctions
 ## because your code would be overwritten at each plugin update
 ###
 
-var particles: Array[GPUParticles2D];
+# Changed to Node2D to allow use of VFXSprite2D.
+var particles: Array[Node2D];
 
 func back_to_grave_deferred(bID):
 	var par = bID.get_parent();
@@ -21,12 +22,6 @@ func delete_bullet_outside(bullet):
 		Spawning.delete_bullet(bullet);
 	if bullet["position"].y < -0 or bullet["position"].y > 10:
 		Spawning.delete_bullet(bullet);
-
-func _on_hit_particle_finished(particle):
-	if particle in get_children():
-		particle.emitting = false;
-		remove_child(particle);
-		particles.append(particle)
 
 func bullet_collide_body(body_rid:RID,body:Node,body_shape_index:int,local_shape_index:int,shared_area:Area2D, B:Dictionary, b:RID) -> void:
 	## you can use B["props"]["damage"] to get the bullet's damage
@@ -41,12 +36,17 @@ func bullet_collide_body(body_rid:RID,body:Node,body_shape_index:int,local_shape
 			body.apply_damage(damage, knockback, B["position"], -body.velocity);
 		else:
 			body.apply_damage(damage, knockback, B["position"], Vector2.ZERO);
+		
 		B["props"]["remaining_pierce"] -= 1;
 		if B["props"]["remaining_pierce"] <= 0:
 			var hit_vfx;
 			if particles.size() < 500:
 				hit_vfx = B["props"]["hit_vfx"].duplicate();
-				hit_vfx.finished.connect(_on_hit_particle_finished.bind(hit_vfx))
+				# hacky: for supporting vfxsprite2D instead of GPUparticles2D
+				if hit_vfx is GPUParticles2D:
+					hit_vfx.finished.connect(_on_hit_particle_finished.bind(hit_vfx))
+				elif hit_vfx is VFXSprite2D:
+					hit_vfx.animation_finished.connect(_on_hit_particle_finished.bind(hit_vfx))
 				body.get_parent().add_child(hit_vfx);
 			else:
 				hit_vfx = particles.pop_front();
@@ -54,11 +54,19 @@ func bullet_collide_body(body_rid:RID,body:Node,body_shape_index:int,local_shape
 			if hit_vfx:
 				hit_vfx.show();
 				hit_vfx.global_position = (B["position"] as Vector2);
-				hit_vfx.emitting = true
+				if hit_vfx is GPUParticles2D:
+					hit_vfx.emitting = true
+				elif hit_vfx is VFXSprite2D:
+					hit_vfx.play(&"default")
 			Spawning.delete_bullet(b);
-
-
 	pass
+
+func _on_hit_particle_finished(particle):
+	if particle in get_children():
+		if particle is GPUParticles2D:
+			particle.emitting = false
+		remove_child(particle);
+		particles.append(particle)
 
 
 func bullet_collide_area(area_rid:RID,area:Area2D,area_shape_index:int,local_shape_index:int,shared_area:Area2D) -> void:
--- src/player/player_controller.gd
+++ src/player/player_controller.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name PlayerController
 
 #region movement_variables
+@export_category("Movement")
 ## Speed of the player in units per tick.
 @export var speed: float = 220.0
 ## Acceleration of the player in units per tick. Unused.
@@ -14,6 +15,7 @@ var look_direction : Vector2
 #endregion
 
 #region Dash variables
+@export_category("Dash")
 @onready var dash_cooldown_timer: Timer = $DashCooldownTimer
 @onready var nom_dash_aoe: Area2D = $NomDashAoE
 
@@ -52,6 +54,7 @@ var absorb_pts : int = 0 :
 
 ## Points required for upgrade. Mind: this is constant. To make the cost increase
 ## for higher levels, we'll use multipliers.
+@export_category("Upgrading")
 @export var upgrade_threshold : int = 10
 ## Max upgrade level. Starts at zero!!!
 @export var max_level : int = 2
@@ -63,8 +66,10 @@ var absorb_pts : int = 0 :
 #endregion
 
 #region Health
+@export_category("Health")
 var health: float
 @export var max_health: float = 100
+@export var death_vfx: PackedScene = preload("res://prefabs/particles/explode_vfx.tscn")
 #endregion
 
 func _ready():
@@ -119,6 +124,11 @@ func apply_damage(damage: int, knockback: float, global_position: Vector2, direc
 	
 	if health <= 0:
 		print("DIE")
+		var vfx = death_vfx.instantiate()
+		add_child(vfx)
+		vfx.top_level = true
+		vfx.global_position = global_position
+		vfx.play(&"default")
 	
 	# TODO: implement knockback.
 
