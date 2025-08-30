a86db7a85046e4f72f9fde4fe44ef09025a6fc1f
Nathan Estrada <naestrada19@gmail.com>
2025-08-29 17:28:56 -0700

    Working resources preloader

--- src/manager/bullet_manager.gd
+++ src/manager/bullet_manager.gd
@@ -3,12 +3,19 @@ extends Node2D
 @onready var player_node = $Player;
 
 func _ready():
-	EventManager.game_started.connect(_on_game_started);
-	
-func _on_game_started():
-	Spawning.create_pool("PBullet", "0", 2000, false);
-	Spawning.create_pool("ChocBullet", "1", 1000);
-	Spawning.create_pool("SugarBullet", "1", 2000);
-
 	player_node.add_to_group("Player");
+
+	_deferred_ready.call_deferred();
+	
+func _deferred_ready():
+	Spawning.create_pool("PBullet", "0", 2000);
+	Spawning.create_pool("ChocBullet", "1", 2000);
+	Spawning.create_pool("SugarBullet", "1", 2000);
+	
+func _process(delta):
+	pass
+	
+
+	
+
 	
