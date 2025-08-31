7e031f561f3e47d86b1420d6e57001881ff5c1da
Feymood <feymood@pm.me>
2025-08-31 11:33:28 +0200

    Add iframes on level start

--- src/player/player_controller.gd
+++ src/player/player_controller.gd
@@ -230,6 +230,8 @@ func respawn() -> void:
 		"health_max_value": max_health,
 	})
 	$Root/Upgrade.change_state("Level1")
+	# Handle iframes:
+	modifier_handler.add_child(InvulnModifier.new(IFRAME_DURATION*2))
 
 ## Reduces upgrade level by one. 
 func level_down() -> void:
